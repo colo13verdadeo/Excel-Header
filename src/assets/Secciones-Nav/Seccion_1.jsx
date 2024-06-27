5335af4e51b168e7b148672d300996aaf323e6f8
colo13 <joa_028@hotmail.com>
2024-06-27 23:09:06 +0200

    V1.2

--- src/assets/Secciones-Nav/Seccion_1.jsx
+++ src/assets/Secciones-Nav/Seccion_1.jsx
@@ -2,9 +2,9 @@ export function Seccion1()
 {
     return(
         <div className="secccion1">
-            <img src="/vite.svg" className="sec_img1Copiar"></img>
+            <img src="/svg/brocha.svg" className="sec_img1Copiar"></img>
             <span className="sec_span1Copiar">Copiar <br></br> Formato</span>
-            <img src="/vite.svg" className="sec_img2Copiar"></img>
+            <img src="/svg/paste.svg" className="sec_img2Copiar"></img>
             {/* Desplegale */}
             <span className="sec_span2Copiar">Pegar<br></br> Formato</span>
             <img src="/svg/tigeras.svg" className="sec_img3Copiar"></img>
